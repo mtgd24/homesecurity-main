7941b91b14ad705479b14244ce486cca4bc569b7
mtgd24 <hoangcaophong2001@gmail.com>
2025-02-26 14:25:39 +0800

    Initial Commit

---
+++ homesecurity main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homesecurity main
+//
+//  Created by MTGD on 2025/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ homesecurity mainUITests/homesecurity_mainUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  homesecurity_mainUITestsLaunchTests.swift
+//  homesecurity mainUITests
+//
+//  Created by MTGD on 2025/2/26.
+//
+
+import XCTest
+
+final class homesecurity_mainUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
